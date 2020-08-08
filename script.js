98951f3f35642c5c0c7a35b6a00bcce4d2acdf7f
Kevin Apostol <apostolkevin@gmail.com>
2020-08-08 14:26:14 -0700

    Modified the displayResult function

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
  * DONE: Get user value from input and save it to variable numberGuess
  * DONE: Generate a random number 1 to 100 and save it to variable correctNumber
  * DONE: Console whether the guess is too high, too low, or is correct inside playGame function
- * TODO: Create a function called displayResult to move the logic for if the guess is too high, too low, or correct
+ * DONE: Create a function called displayResult to move the logic for if the guess is too high, too low, or correct
  * TODO: Complete the showYouWon, showNumberAbove, showNumberBelow
  * TODO: Use the showYouWon... functions within displayResult to display the correct dialog
  * TODO: Save the guess history in a variable called guess
@@ -30,29 +30,28 @@ window.onload = function () {
  */
 function playGame() {
     let numberGuess = document.getElementById("number-guess").value;
-
-    if (numberGuess > correctNumber) {
-        console.log("Number is too high.");
-    } else if (numberGuess < correctNumber) {
-        console.log("Number is too low.");
-    } else {
-        console.log("Number is Correct");
-    }
+    displayResult(numberGuess);
 }
 
 /**
  * Show the result for if the guess it too high, too low, or correct
  * HINT: Use if, else if, else statement
  */
-// *CODE GOES BELOW HERE *
+function displayResult(numberGuess) {
+    if (numberGuess > correctNumber) {
+        console.log("too high.");
+    } else if (numberGuess < correctNumber) {
+        console.log("too low.");
+    } else {
+        console.log("is Correct");
+    }
+}
 
 /**
  * Initialize a new game by resetting all values and content on the page
  * HINT: reset the correctNumber, guesses, and HTML content
  */
-function initGame() {
-    // *CODE GOES BELOW HERE *
-}
+function initGame() {}
 
 /**
  * Reset the HTML content for guess history
